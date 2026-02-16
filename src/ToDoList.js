e29638941e607b0cf83d2a3bb825f90627d33562
Rachel <rachel.sun.rondon@gmail.com>
2026-02-16 18:59:11 -0500

    feat: update ToDoList

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,9 @@
 import { useState } from 'react';
 
 const ToDoList = () => {
-  
+
   const [toDo, setToDo] = useState("");
   const [toDoList, setToDoList] = useState([]);
-
-  console.log('toDo', toDo);
-  console.log('toDoList', toDoList);
 
   const addToDo = (e) => {
     e.preventDefault();
@@ -15,21 +12,30 @@ const ToDoList = () => {
     setToDo("");
   };
 
-  console.log(toDoList);
-
   return (
     <div>
-      <form onSubmit={addToDo}>
-        <input 
-          value={toDo}
-          name="to do"
-          onChange={(e) => {
-            setToDo(e.target.value);
-          }}
-        />
-        <button type="submit" />
-      </form>
-      </div>
+        <form onSubmit={addToDo}>
+          <input 
+            value={toDo}
+            name="to do"
+            onChange={(e) => {
+              setToDo(e.target.value);
+            }}
+          />
+          <button type="submit" />
+        </form>
+        {toDoList.map((item) => {
+          return (
+            <div className="to-do">
+            <h2>{item}</h2>
+            <button 
+              className="remove">
+                remove
+              </button>
+            </div>      
+          )
+        })}
+    </div>
   )
 };
 
